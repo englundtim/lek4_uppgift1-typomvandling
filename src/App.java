c8949b2e003cf3ab40c3bb73c4ecdbc2ddbeaf9b
Tim <tim.englund@elev.ga.ntig.se>
2023-09-11 09:15:20 +0200

    Lade till divisionsberäkning

--- src/App.java
+++ src/App.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Uppgift a)");
-        
+        Scanner tal = new Scanner(System.in);
+        System.out.println("Skriv in ditt första heltal");
+        int tal1 = tal.nextInt();
+        System.out.println("Skiirv in ditt andra heltal");
+        int tal2 = tal.nextInt();
+
+        double kvot_dec = (double)tal1/tal2;
+        System.out.println("kvoten är: "+kvot_dec);
+        int kvot_hel = tal1/tal2;
+        System.out.println("kvoten är: "+kvot_hel);
+
     }
 }
